d3a869525c4065653266a3d75ee8b45c3c780e7b
Jorge Escamilla <corem96@gmail.com>
2019-02-11 23:49:44 -0700

    Add implementation of IDesignTimeDbContextFactory for execute ef commands from Data project

---
+++ src/Vega.WebApi/Config/DesignTimeDbContextFactory.cs
@@ -0,0 +1,46 @@
+using System.IO;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.Configuration;
+using Vega.Data.Context;
+
+namespace Vega.WebApi.Config
+{
+    /*
+     * Problem
+     * If DbContext is in a separate project – class library project.
+     * to add new migration and update database, may encounter this error:
+     *
+     * -- Unable to create an object of type ‘CodingBlastDbContext’.
+     * -- Add an implementation of ‘IDesignTimeDbContextFactory’ to the project,
+     * -- or see https://go.microsoft.com/fwlink/?linkid=851728
+     * -- for additional patterns supported at design time.
+     *
+     * Solution
+     * Add a class that implements IDesignTimeDbContextFactory inside of Web project.
+     *
+     * Navigate to Database project and run the following from command line:
+     * > dotnet ef migrations add <migration-name> -s ../<web-project-folder>/
+     * > dotnet ef database update -s ../<web-project-folder>/
+     *
+     * -s stands for startup project and ../<web-project-folder>/ is the location of my web/startup project.
+     *
+     * Github page of this solution: https://github.com/Ibro/DesignTimeDbContextFactory
+     */
+    
+    public class DesignTimeDbContextFactory : IDesignTimeDbContextFactory<ApplicationDbContext>
+    {
+        public ApplicationDbContext CreateDbContext(string[] args)
+        {
+            IConfigurationRoot configuration = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json")
+                .Build();
+
+            var builder = new DbContextOptionsBuilder<ApplicationDbContext>();
+            builder.UseSqlServer(configuration.GetConnectionString("Default"));
+            
+            return new ApplicationDbContext(builder.Options);
+        }
+    }
+}
