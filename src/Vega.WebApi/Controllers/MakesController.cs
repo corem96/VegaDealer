196e19815dace16b02f138c6fd95df92874a2dbc
Jorge Escamilla <corem96@gmail.com>
2019-04-03 22:07:56 -0700

    Controller return mapped Make model object to client

--- src/Vega.WebApi/Controllers/MakesController.cs
+++ src/Vega.WebApi/Controllers/MakesController.cs
@@ -1,27 +1,33 @@
 using System.Collections;
 using System.Collections.Generic;
 using System.Threading.Tasks;
+using AutoMapper;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Vega.Data.Context;
 using Vega.Domain.Models;
+using Vega.WebApi.Controllers.Resources;
 
 namespace Vega.WebApi.Controllers
 {
     public class MakesController : Controller
     {
         private readonly ApplicationDbContext _context;
+        private readonly IMapper _mapper;
 
-        public MakesController(ApplicationDbContext context)
+        public MakesController(ApplicationDbContext context, IMapper mapper)
         {
             _context = context;
+            _mapper = mapper;
         }
         
         [HttpGet("/api/makes")]
-        public async Task<IEnumerable<Make>> GetMakes()
+        public async Task<IEnumerable<MakeResource>> GetMakes()
         {
-            return await _context.Makes.Include(m => m.Models)
+            var makes = await _context.Makes.Include(m => m.Models)
                 .ToListAsync();
+
+            return _mapper.Map<List<Make>, List<MakeResource>>(makes);
         }
     }
 }
